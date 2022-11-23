a68be5333f27eb42982131372167f3c480d99de3
Guilherme <guigomes.ti@gmail.com>
2022-11-23 16:36:16 -0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'laudo-hidroimagem',
+    appId: '1:646662881915:web:a42074bf56993a51f758de',
+    databaseURL: 'https://laudo-hidroimagem-default-rtdb.firebaseio.com',
+    storageBucket: 'laudo-hidroimagem.appspot.com',
+    apiKey: 'AIzaSyDVBdw6kmFlpT_S-AWLZ2amumx9i7tT8rg',
+    authDomain: 'laudo-hidroimagem.firebaseapp.com',
+    messagingSenderId: '646662881915',
+    measurementId: 'G-91F9XFT5BE',
+  }
+  };
